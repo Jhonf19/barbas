2cc56ee314d35dccd6cae461723847e214293134
jhonf19 <zjhon2018@gmail.com>
2019-09-18 17:30:20 -0500

    view citas barber almost done

--- views/barber/list_citas.php
+++ views/barber/list_citas.php
@@ -1,21 +1,26 @@
 <?php
-//echo "<pre>";print_r($res);echo "</pre>";
+// echo "<pre>";print_r($res);echo "</pre>";
 
  ?>
+ <br>
 <div class="container">
+  <h3 class="text-center">Agenda de Citas</h3><br>
   <div class="table-responsive">
     <table class="table table-bordered text-center">
       <thead>
-        <th>FECHA Y HORA</th> <th>CLIENTE</th><th><i class="fas fa-cog"></i> </th>
+         <th>CLIENTE</th><th>FECHA Y HORA</th><th><i class="fas fa-cog"></i> </th>
       </thead>
       <tbody>
         <?php foreach ($res as $key => $row): ?>
         <tr>
           <td>
+            <?php echo $row->nombre." ".$row->apellido; ?>
+          </td>
+          <td>
             <?php echo $row->dia."/".$row->mes."/".$row->anio." a las ".$row->hora; ?>
           </td>
           <td>
-            <?php echo $row->nombre." ".$row->apellido; ?>
+            <a class="btn btn-success" href="#"><i class="fas fa-check"></i> </a>
           </td>
         </tr>
       <?php endforeach; ?>
